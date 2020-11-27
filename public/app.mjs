3abd2fce10890c32c50e105ad20263b45262d18a
Tom Kelly <tmkelly28@gmail.com>
2020-11-27 16:54:11 -0500

    made a singe wave

--- public/app.mjs
+++ public/app.mjs
@@ -1,4 +1,5 @@
 import World from './world.mjs'
+import Circle from './circle.mjs'
 
 const init = () => {
   const canvas = document.getElementById('canvas')
@@ -6,6 +7,14 @@ const init = () => {
   canvas.height = window.innerHeight
 
   const world = new World(canvas)
+  const circle = new Circle({
+    x: 10,
+    y: canvas.height / 2,
+    radius: 10,
+    color: 'burlywood',
+  })
+
+  world.add(circle)
 
   window.addEventListener('resize', () => world.resize(), true)
   window.addEventListener('mousemove', ({ clientX, clientY }) => {
--- public/circle.mjs
+++ public/circle.mjs
@@ -2,12 +2,37 @@ export default class Circle {
   constructor({ x, y, radius, color }) {
     this.x = x
     this.y = y
+    this.dx = 1
+    this.dy = 0
     this.radius = radius
     this.color = color
   }
 
   update(world) {
     this.draw(world)
+
+    const { canvas } = world
+
+    if (this.x >= canvas.width + this.radius) {
+      this.x = 0
+    }
+
+    if (this.y >= canvas.height + this.radius) {
+      this.y = 0
+    }
+
+    if (this.y <= 0 - this.radius) {
+      this.y = canvas.height
+    }
+
+    this.x += this.dx
+
+    const l = 100 // wavelength
+    const p = Math.PI * 2
+    const k = p / l // spatial angular frequency (wavenumber)
+    const a = 3 // amplitude
+    this.dy = a * Math.sin(k * this.x - p)
+    this.y += this.dy
   }
 
   draw(world) {
