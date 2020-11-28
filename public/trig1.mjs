87f6fde5b0d1ef0f5b5d15a6a4cc2e1ae8f27d34
Tom Kelly <tom@getcatalyst.io>
2020-11-28 13:58:30 -0500

    prep for other examples

--- public/trig1.mjs
+++ public/trig1.mjs
@@ -5,9 +5,24 @@ const mouse = { x: 0, y: 0 }
 canvas.width = window.innerWidth
 canvas.height = window.innerHeight
 
+// converting radians/degrees
+const radianToDegree = r => (r * 180) / Math.PI
+const degreeToRadian = d => (d * Math.PI) / 180
+
+ctx.translate(0, canvas.height / 2)
+ctx.scale(1, -1)
+
 const animate = () => {
   window.requestAnimationFrame(animate)
   ctx.clearRect(0, 0, canvas.width, canvas.height)
+
+  for (let angle = 0; angle < Math.PI * 2; angle += 0.01) {
+    const scale = 100
+    const x = angle * scale
+    const y = Math.sin(angle) * scale
+
+    ctx.fillRect(x, y, 1, 1)
+  }
 }
 
 const onResize = () => {
@@ -23,3 +38,4 @@ const onMouseMove = ({ clientX, clientY }) => {
 window.addEventListener('resize', onResize, true)
 
 animate()
+
