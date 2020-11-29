1b87afaf9df39812e26ebbd5f2aaf262d87e505f
Tom Kelly <tom@getcatalyst.io>
2020-11-29 10:46:39 -0500

    more examples for main, css

--- public/circle.mjs
+++ public/circle.mjs
@@ -17,7 +17,17 @@ export default class Circle {
     ctx.beginPath()
     ctx.arc(x, y, radius, 0, Math.PI * 2, true)
     ctx.closePath()
+
+    this.stroke(ctx, color)
+  }
+
+  stroke(ctx, color) {
     ctx.strokeStyle = color
     ctx.stroke()
+  }
+
+  fill(ctx, color) {
+    ctx.fillStyle = color
+    ctx.fill()
   }
 }
--- public/rect.mjs
+++ public/rect.mjs
@@ -12,11 +12,23 @@ export default class Rect {
   }
 
   draw(world) {
+    this.stroke(world)
+  }
+
+  fill(world) {
     const { ctx } = world
     const { x, y, color, width, height } = this
 
     ctx.fillStyle = color
     ctx.fillRect(x, y, width, height)
+  }
+
+  stroke(world) {
+    const { ctx } = world
+    const { x, y, color, width, height } = this
+
+    ctx.strokeStyle = color
+    ctx.strokeRect(x, y, width, height)
   }
 }
 
