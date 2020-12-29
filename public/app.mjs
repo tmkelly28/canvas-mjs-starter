f3d9ff5a05f1542b7e4169fcc42cbcfd0d73739b
Tom Kelly <tom@getcatalyst.io>
2020-12-29 17:49:06 -0500

    sort of a camera

--- public/app.mjs
+++ public/app.mjs
@@ -1,21 +1,43 @@
-import World from './world.mjs'
-
 const init = () => {
   const canvas = document.getElementById('canvas')
-  canvas.width = window.innerWidth
-  canvas.height = window.innerHeight
+  const context = canvas.getContext('2d')
 
-  const world = new World(canvas)
+  canvas.width = 500
+  canvas.height = 500
 
-  window.addEventListener('resize', () => world.resize(), true)
-  window.addEventListener('mousemove', ({ clientX, clientY }) => {
-    world.updateMouse(clientX, clientY)
-  }, false)
-  window.world = world
+  const camera = {
+    x: 5, y: 5
+  }
+
+  window.addEventListener('keydown', ({ code }) => {
+    switch(code) {
+      case 'KeyA':
+        camera.x += 5
+        break
+      case 'KeyW':
+        camera.y += 5
+        break
+      case 'KeyD':
+        camera.x -= 5
+        break
+      case 'KeyS':
+        camera.y -= 5
+        break
+    }
+  })
 
   const animate = () => {
+    context.setTransform(1, 0, 0, 1, 0, 0)
+    context.clearRect(0, 0, canvas.width, canvas.height)
+
+    context.translate(camera.x, camera.y)
+
+    context.beginPath()
+    context.arc(500, 500, 100, 0, Math.PI * 2, false)
+    context.closePath()
+    context.fill()
+
     window.requestAnimationFrame(animate)
-    world.update()
   }
 
   animate()
