a745ae12bae6348bdfc0ce1f49a4f03c84220fd5
Tom Kelly <tom@getcatalyst.io>
2021-01-11 15:01:08 -0500

    whoops

--- public/app.mjs
+++ public/app.mjs
@@ -1,14 +1,16 @@
 const init = () => {
   const canvas = document.getElementById('canvas')
+  const context = canvas.getContext('2d')
   canvas.width = window.innerWidth
   canvas.height = window.innerHeight
 
   window.addEventListener('resize', () => {
-    this.canvas.width = window.innerWidth
-    this.canvas.height = window.innerHeight
+    canvas.width = window.innerWidth
+    canvas.height = window.innerHeight
   }, false)
 
   const animate = () => {
+    context.clearRect(0, 0, canvas.width, canvas.height)
     window.requestAnimationFrame(animate)
   }
 
